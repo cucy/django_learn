ac3d665f02ad35ff5df0e6ed4e760970b8f411be
zrd <292016176@qq.com>
2018-06-25 01:02:22 +0800

    修改密码

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -15,4 +15,11 @@ urlpatterns = [
     path('', views.dashboard, name='dashboard'),
     path('login/', auth_views.LoginView.as_view(), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
+
+    path('password_change/',
+         auth_views.PasswordChangeView.as_view(template_name="registration/password_change_form1.html"),
+         name='password_change', ),
+    path('password_change/done/',
+         auth_views.PasswordChangeDoneView.as_view(template_name="registration/password_change_done1.html"),
+         name='password_change_done'),
 ]
