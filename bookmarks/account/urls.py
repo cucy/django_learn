5093938e238827a9d5312e01514891b6661adc68
zrd <292016176@qq.com>
2018-06-25 01:19:54 +0800

    密码重置

--- bookmarks/account/urls.py
+++ bookmarks/account/urls.py
@@ -13,7 +13,7 @@ urlpatterns = [
     # path('login/', views.user_login, name='login'),
 
     path('', views.dashboard, name='dashboard'),
-    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name="registration/login1.html"), name='login'),
     path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 
     path('password_change/',
@@ -22,4 +22,21 @@ urlpatterns = [
     path('password_change/done/',
          auth_views.PasswordChangeDoneView.as_view(template_name="registration/password_change_done1.html"),
          name='password_change_done'),
+
+    #  修改密码
+    path('password_reset/',  # 重置密码页面
+         auth_views.PasswordResetView.as_view(
+             template_name="registration/password_reset_form1.html",
+             email_template_name="registration/password_reset_email1.html"
+         ),
+         name='password_reset'),
+    path('password_reset/done/',  # 发送邮件成功后显示的页面
+         auth_views.PasswordResetDoneView.as_view(template_name="registration/password_reset_done1.html"),
+         name='password_reset_done'),
+    path('reset/<uidb64>/<token>/',  # 邮件链接跳转到此页面
+         auth_views.PasswordResetConfirmView.as_view(template_name="registration/password_reset_confirm1.html"),
+         name='password_reset_confirm'),
+    path('reset/done/',   # 修改密码成功页面
+         auth_views.PasswordResetCompleteView.as_view(template_name="registration/password_reset_complete1.html"),
+         name='password_reset_complete'),
 ]
